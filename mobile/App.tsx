11e72bec9cd38a900f808668b9915b9fda5300ef
Pedro Soares <pncsoares@gmail.com>
2022-10-25 08:38:14 +0100

    Create mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>NLW mobile!</Text>
+            <StatusBar style='auto' />
+        </View>
+    );
+}
+
+interface ButtonProps {
+    title: string;
+}
+
+function Button(props: ButtonProps) {
+    return (
+        <TouchableOpacity>
+            <Text>{props.title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {},
+});
